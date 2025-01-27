4821d269ad8fce5c10a3c3c7e30aa2e3c6a9267d
ayanchavand <ayan.chavand15@gmail.com>
2025-01-27 16:14:22 +0530

    [BASIC] margin and padding

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       {/*Centering a div using flex*/}
       <div className="flex justify-center items-center h-screen">
-        <div className="text-center space-y-4">
+        <div className="text-center space-y-4 p-4 m-4">
           <h1 className="bg-amber-200 text-8xl ">TITLE</h1>
           <h2 className="bg-amber-200 text-2xl">SUBheading</h2>
           <button className="btn-primary">
@@ -14,12 +14,6 @@ function App() {
           </button>
 
           <p className="font-bold text-3xl leading-relaxed">asdfsda askdfhsdjk</p>
-        </div>
-        <div className="">
-          <div></div>
-        </div>
-        <div>
-          <h1>h11</h1>
         </div>
       </div>
     </>
