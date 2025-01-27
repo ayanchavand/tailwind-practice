b8c79e68e04f89c6dcbb53227092ac0464ca0391
ayanchavand <ayan.chavand15@gmail.com>
2025-01-27 16:00:07 +0530

    [BASIC] adding button and basic hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   return (
     <>
     {/*Centering a div using flex*/}
-    <div className='flex justify-center items-center h-screen '>
-    <div className='text-center'>
+    <div className='flex justify-center items-center h-screen'>
+    <div className='text-center space-y-4'>
     <h1 className='bg-amber-200 text-8xl '>TITLE</h1>
-    <h1 className='bg-amber-200 text-2xl'>SUBheading</h1>
+    <h2 className='bg-amber-200 text-2xl'>SUBheading</h2>
+    <button className='bg-amber-700 h-8 rounded outline-1 outline-amber-300 hover:bg-amber-400 w-16'>hii</button>
     </div>
     </div>
       
