72730507e19d4ffb3c2d3918f3c0fe39e275c4d0
ayanchavand <ayan.chavand15@gmail.com>
2025-01-27 16:31:09 +0530

    [BASIC] grid change using media query

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       {/*Centering a div using flex*/}
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center h-screen">
         <div className="text-center space-y-4 p-4 m-4">
           <h1 className="bg-amber-200 text-8xl ">TITLE</h1>
           <h2 className="bg-amber-200 text-2xl">SUBheading</h2>
@@ -17,7 +17,12 @@ function App() {
         <div className="text-center p-6 w-lg h-44 bg-slate-400 rounded-2xl border shadow-md ">
           <h1>Name</h1>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 my-3 bg-amber-100">
+        <a>1</a>
+        <a>2</a>
       </div>
+      </div>
+      
     </>
   );
 }
